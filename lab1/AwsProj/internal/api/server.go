ecf61443897b304ff801b0fa9aba721c9b507035
shPac-Men <dlya-igor-artem@yandex.ru>
2025-09-27 20:54:28 +0300

    changed css and names

--- lab1/AwsProj/internal/api/server.go
+++ lab1/AwsProj/internal/api/server.go
@@ -1,55 +1,3 @@
-// package api
-
-// import (
-// 	"AwsProj/internal/app/handler"
-// 	"AwsProj/internal/app/repository"
-// 	"log"
-
-// 	"github.com/gin-gonic/gin"
-// 	"github.com/sirupsen/logrus"
-// )
-
-// func StartServer() {
-// 	log.Println("Starting server")
-
-// 	repo, err := repository.NewRepository()
-// 	if err != nil {
-// 		logrus.Error("ошибка инициализации репозитория")
-// 	}
-
-// 	handler := handler.NewHandler(repo)
-
-// 	r := gin.Default()
-// 	// добавляем наш html/шаблон
-// 	r.LoadHTMLGlob("../../templates/*")
-
-// 	r.GET("/hello", handler.GetOrders)
-
-// 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
-// 	log.Println("Server down")
-// }
-
-// func StartServer() {
-// 	log.Println("Starting server")
-
-// 	repo, err := repository.NewRepository()
-// 	if err != nil {
-// 		logrus.Error("ошибка инициализации репозитория")
-// 	}
-
-// 	handler := handler.NewHandler(repo)
-
-// 	r := gin.Default()
-// 	// добавляем наш html/шаблон
-// 	r.LoadHTMLGlob("../../templates/*")
-
-// 	r.GET("/hello", handler.GetOrders)
-// 	r.GET("/order/:id", handler.GetOrder) // вот наш новый обработчик
-
-// 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
-// 	log.Println("Server down")
-// }
-
 package api
 
 import (
@@ -78,7 +26,7 @@ func StartServer() {
 	// слева название папки, в которую выгрузится наша статика
 	// справа путь к папке, в которой лежит статика
 
-	r.GET("/hello", handler.GetOrders)
+	r.GET("/chemistry", handler.GetOrders)
 	r.GET("/order/:id", handler.GetOrder)
 	r.GET("/calculatepage", handler.CalculatePage)
 
